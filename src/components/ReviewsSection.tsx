9bfe22d0b680477f4fe67cad9c0c17f6d94c41f2
Denis <zalizkodenis12@gmail.com>
2026-08-23 00:17:38 +0300

    style: remove decorative quote icon from review cards

--- src/components/ReviewsSection.tsx
+++ src/components/ReviewsSection.tsx
@@ -108,12 +108,6 @@ export default function ReviewsSection() {
                       <p className="text-[#FFC107] dark:text-[#E0A500] font-bold uppercase tracking-widest text-sm mt-2">{review.role}</p>
                     </div>
                     
-                    {/* Quote icon background decoration */}
-                    <div className="absolute bottom-6 right-6 opacity-[0.03] group-hover:opacity-[0.06] transition-opacity duration-300 pointer-events-none">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-32 h-32">
-                        <path fillRule="evenodd" d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.43 3.43 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" clipRule="evenodd" />
-                      </svg>
-                    </div>
                   </div>
                 </div>
               </SwiperSlide>
@@ -150,12 +144,6 @@ export default function ReviewsSection() {
                 <p className="text-[#FFC107] dark:text-[#E0A500] font-bold uppercase tracking-widest text-sm mt-2">{review.role}</p>
               </div>
               
-              {/* Quote icon background decoration */}
-              <div className="absolute bottom-6 right-6 opacity-[0.03] group-hover:opacity-[0.06] transition-opacity duration-300 pointer-events-none">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-32 h-32">
-                  <path fillRule="evenodd" d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.43 3.43 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" clipRule="evenodd" />
-                </svg>
-              </div>
             </motion.div>
           ))}
         </div>
